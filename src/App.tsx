299a0209d12a23a46fcce9fefa819983aa2b7425
Emiljan Dusha <emiljanthegreat@gmail.com>
2023-10-10 12:01:17 +0200

    added infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,44 @@
-import { useEffect } from 'react'
-import './App.css'
+import { useCallback, useEffect } from 'react'
 import Gallery from './components/Gallery'
 import FilterSection from './components/FilterSection'
 import { useAppSelector } from './libs/store'
 import { selectPage, setPage } from './slices/filterSlice'
 import { useDispatch } from 'react-redux'
-import { fetchGallery, selectGallery } from './slices/gallerySlice'
+import {
+  fetchGallery,
+  selectGallery,
+  selectIsLoading,
+} from './slices/gallerySlice'
 import { add, compose } from 'ramda'
+import useInfiniteScroll from 'react-infinite-scroll-hook'
 
 function App() {
-  const page = useAppSelector(selectPage)
-  const gallery = useAppSelector(selectGallery)
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(fetchGallery(page))
-  }, [page, dispatch])
+  const page = useAppSelector(selectPage)
+  const gallery = useAppSelector(selectGallery)
+  const isLoading = useAppSelector(selectIsLoading)
 
-  // no need for callback as page is the only thing that will make the component rerender so there's nothing we can save for efficiency
-  const nextPage = () => compose(dispatch, setPage, add(1))(page)
+  const nextPage = useCallback(
+    () => compose(dispatch, setPage, add(1))(page),
+    [page, dispatch]
+  )
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: isLoading,
+    hasNextPage: true,
+    onLoadMore: nextPage,
+    disabled: false,
+    rootMargin: '0px 0px 500px 0px',
+  })
+
+  useEffect(() => dispatch(fetchGallery(page)), [page, dispatch])
 
   return (
-    <>
+    <div ref={sentryRef}>
       <FilterSection />
       <Gallery gallery={gallery} />
-      <div onClick={nextPage}>mbushe</div>
-    </>
+    </div>
   )
 }
 
